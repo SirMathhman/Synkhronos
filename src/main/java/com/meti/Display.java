cb112146e04228118ee83b3c0ae7e42c6021f7e3
SirMathhman <minecraftmathhman@gmail.com>
2018-01-31 08:11:48 -0500

    FXML methods

--- src/main/java/com/meti/Display.java
+++ src/main/java/com/meti/Display.java
@@ -1,4 +1,41 @@
 package com.meti;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class Display {
+    @FXML
+    private ListView<String> dependencyView;
+
+    @FXML
+    public void openList(){
+    }
+
+    @FXML
+    public void saveList(){
+    }
+
+    @FXML
+    public void openSettings(){
+    }
+
+    @FXML
+    public void close(){
+    }
+
+    @FXML
+    public void addDependency(){
+    }
+
+    @FXML
+    public void removeDependency(){
+    }
+
+    @FXML
+    public void clearDependencies(){
+    }
+
+    @FXML
+    public void openDependencySettings(){
+    }
 }
